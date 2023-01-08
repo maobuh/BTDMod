d3641f99b9824ebf3ee42f559c4fb12f2569fc9e
maobuh <chen.ryan.syd@hotmail.com>
2023-01-08 20:37:36 +1100

    ultrajuggernaut balls split

--- Projectiles/Juggernaut.cs
+++ Projectiles/Juggernaut.cs
@@ -3,11 +3,13 @@ using Terraria;
 using Terraria.ID;
 using Terraria.Audio;
 using Terraria.ModLoader;
+using System;
 
 namespace BTDMod.Projectiles
 {
     class Juggernaut : ModProjectile
     {
+        int totalCollisions;
         public override void SetDefaults()
         {
             Projectile.DamageType = DamageClass.Ranged;
@@ -21,8 +23,22 @@ namespace BTDMod.Projectiles
         }
         public override bool OnTileCollide(Vector2 oldVelocity)
         {
+            totalCollisions++;
+            Collision.HitTiles(Projectile.position, Projectile.velocity, Projectile.width, Projectile.height);
 			SoundEngine.PlaySound(SoundID.Item10, Projectile.position);
-            return true;
+            // If the projectile hits the left or right side of the tile, reverse the X velocity
+            if (Math.Abs(Projectile.velocity.X - oldVelocity.X) > float.Epsilon) {
+                Projectile.velocity.X = -oldVelocity.X;
+            }
+            // If the projectile hits the top or bottom side of the tile, reverse the Y velocity
+            if (Math.Abs(Projectile.velocity.Y - oldVelocity.Y) > float.Epsilon) {
+                Projectile.velocity.Y = -oldVelocity.Y;
+            }
+            if (totalCollisions > 2) {
+                Projectile.Kill();
+            }
+			SoundEngine.PlaySound(SoundID.Item10, Projectile.position);
+            return false;
         }
     }
 }
