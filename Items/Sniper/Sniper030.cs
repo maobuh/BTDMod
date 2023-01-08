4f3e8486a3e98cd637b895b4b85b695a51ceba38
Chris Ai <christopher.ai666@gmail.com>
2023-01-09 00:29:40 +1100

    sniper base + bouncing bullet done

---
+++ Items/Sniper/Sniper030.cs
@@ -0,0 +1,36 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using BTDMod.Projectiles;
+
+namespace BTDMod.Items.Sniper
+{
+    class Sniper030: ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.CloneDefaults(ModContent.ItemType<Sniper000>());
+            Item.damage = 30;
+            Item.rare = ItemRarityID.Green;
+            Item.shoot = ModContent.ProjectileType<BouncingBullet>();
+        }
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Sniper Monkey");
+            Tooltip.SetDefault("0-3-0 Sniper");
+        }
+        public override void AddRecipes()
+        {
+            Recipe recipe = Recipe.Create(Item.type);
+            recipe.AddIngredient(null, "Sniper000", 1);
+
+            Recipe recipe2 = recipe.Clone();
+
+            recipe.AddIngredient(ItemID.DemoniteBar, 5);
+            recipe.Register();
+
+            recipe2.AddIngredient(ItemID.CrimtaneBar, 5);
+            recipe2.Register();
+        }
+    }
+}
