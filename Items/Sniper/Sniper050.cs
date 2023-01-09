98de97c207fb8140fb96a197a488e0ab061147e9
Chris Ai <christopher.ai666@gmail.com>
2023-01-10 01:29:11 +1100

    finished 050 Sniper

--- Items/Sniper/Sniper050.cs
+++ Items/Sniper/Sniper050.cs
@@ -10,7 +10,7 @@ using System;
 
 namespace BTDMod.Items.Sniper
 {
-    class Sniper040: ModItem
+    class Sniper050: ModItem
     {
         const int ABILITY_COOLDOWN = 10800;
         const int SCREENWIDTH = 1920;
@@ -18,14 +18,14 @@ namespace BTDMod.Items.Sniper
         public override void SetDefaults()
         {
             Item.CloneDefaults(ModContent.ItemType<Sniper000>());
-            Item.damage = 60;
+            Item.damage = 100;
             Item.rare = ItemRarityID.LightRed;
             Item.shoot = ModContent.ProjectileType<BouncingBullet>();
         }
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Sniper Monkey 0-4-0");
-            Tooltip.SetDefault("Bullets bounce up to 3 times on nearby enemies\nBullets gain 20% damage every bounce\nAbility summons a Supply Drop that drops hearts upon death");
+            DisplayName.SetDefault("Sniper Monkey 0-5-0");
+            Tooltip.SetDefault("Bullets bounce up to 3 times on nearby enemies\nBullets gain 20% damage every bounce\nAbility summons a Supply Drop that drops hearts upon death\nGrants a buff if a bullet bounces the maximum number of times");
         }
         public override void HoldItem(Player player)
         {
@@ -37,21 +37,15 @@ namespace BTDMod.Items.Sniper
         }
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
-            Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI, 1);
+            Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI, 2);
             return false;
         }
         public override void AddRecipes()
         {
             Recipe recipe = Recipe.Create(Item.type);
-            recipe.AddIngredient(null, "Sniper030", 1);
-
-            Recipe recipe2 = recipe.Clone();
-
-            recipe.AddIngredient(ItemID.MythrilBar, 5);
+            recipe.AddIngredient(null, "Sniper040", 1);
+            recipe.AddIngredient(ItemID.ChlorophyteBar, 5);
             recipe.Register();
-
-            recipe2.AddIngredient(ItemID.OrichalcumBar, 5);
-            recipe2.Register();
         }
     }
 }
